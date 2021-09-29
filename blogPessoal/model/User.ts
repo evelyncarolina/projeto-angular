5144b4fe215774148e046d8fa04baaafe6c17cc8
evelyncarolina <evelyncarolina.contato@gmail.com>
2021-09-29 20:18:58 -0300

    [Feature Evelyn - Task: Created API Consumption - Model and Service]

---
+++ blogPessoal/model/User.ts
@@ -0,0 +1,8 @@
+export class User {
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public foto: string
+    public tipo: string
+}
--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -18,7 +19,8 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
