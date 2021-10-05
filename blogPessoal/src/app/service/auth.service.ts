9b1604b127e4486fb4e4b4ed8b63591680626b0b
evelyncarolina <evelyncarolina.contato@gmail.com>
2021-10-05 18:16:24 -0300

    Feature Evelyn - Task: Created & Update Component Inicio

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { User } from 'model/User';
 import { UserLogin } from 'model/UserLogin';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,15 @@ export class AuthService {
 
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('https://bloggeneration29.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
